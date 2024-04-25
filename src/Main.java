0ecf3cd99fb2fff26a3bd22be23786a984b16d48
иван <xaxolgazarov@mail.ru>
2024-04-25 14:11:45 +0300

    commite mesage

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] salary = {35000, 40000, 60000, 70000, 90000};
+        int month = 0;
+        for (int i = 0; i < salary.length; i++) {
+            month = month + salary[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + month + " рублей");
+        int min = salary[0];
+        for (int i = 0; i < salary.length; i++) {
+            if (salary[i] < min) {
+                min = salary[i];
+            }
+        }
+        System.out.println("Минимальнаясумма трат за неделю составила " + min + " рублей");
+
+        int max = salary[0];
+        for (int i = 0; i < salary.length; i++) {
+            if (salary[i] > max) {
+                max = salary[i];
+            }
+        }
+        System.out.println("Максимальная сумма трат за неделю составила " + max + " рублей");
+        double medium = 0;
+        for (int i = 0; i < salary.length; i++) {
+            medium += salary[i] / salary.length;
+        }
+        System.out.println("Средняя сумма трат за месяц составила " + medium + " рублей");
+
+        char reverseFullName[] = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+            for (int i = 0; i < reverseFullName.length /2; i++) {
+                int a = reverseFullName[i];
+                reverseFullName[i] = reverseFullName[reverseFullName.length - 1-i];
+                reverseFullName[reverseFullName.length - 1-i] = (char) a;
+            }
+            for (int i = 0; i < reverseFullName.length; i++) {
+                System.out.print(reverseFullName[i] + " ");
+            }
+        }
     }
-}
+
+
+
